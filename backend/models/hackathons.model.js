72de41c10284acc33b12dd2f1ec452c45145ef8a
Rutik <rutikraundale5@gmail.com>
2026-04-13 00:29:41 +0530

    Add DB Models

---
+++ backend/models/hackathons.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const hackathonSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    teamsize: {
+        type: Number,
+        required: true
+    },
+    prizes: {
+        type: [String],
+        required: false
+    },
+    
+    
+    
+    
+}, { timestamps: true });
+
+const Hackathon = mongoose.model("Hackathon", hackathonSchema);
+
+export default Hackathon;
---
+++ backend/models/user.model.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profilePicture: {
+        type: String,
+        required: false
+    },
+    bio: {
+        type: String,
+        required: false
+    },
+    college: {
+        type: String,
+        required: false
+    },
+    skills: {
+        type: [String],
+        required: false
+    },
+    projects: {
+        type: [String],
+        required: false
+    },
+    hackathonsParticipated: {
+        type: [String],
+        required: false
+    },
+    hackathonsWon: {
+        type: [String],
+        required: false
+    },
+    codingPlatforms: {
+        type: [String],
+        required: false
+    },
+    socialLinks: {
+        type: [String],
+        required: false
+    },
+    isProfileCompleted: {
+        type: Boolean,
+        default: false
+    },
+    
+    
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
